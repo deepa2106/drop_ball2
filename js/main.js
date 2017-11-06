9eb899c5a93c91275275fde428d0a1b7b3062eeb
Tech-A03 <tech-a03@Tech-A03s-MacBook-Air-2.local>
2017-11-06 15:28:33 +0000

    Screen scrolls up twice automatically

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(function() {
   console.log("DOM is loaded");
-
+  function reset() {
+    $(".first").slideUp(3000, function() {
+      $(".second").slideUp(3000, reset);
+    });
+  }
+  reset();
 })
