6659ecd3b9c941c1d8326f0fdc8885fe387dde60
Tech-A03 <tech-a03@Tech-A03s-MacBook-Air-2.local>
2017-11-07 11:48:07 +0000

    Ball moves left

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 $(function() {
   console.log("DOM is loaded");
   function reset() {
-    $(".line").animate({top: '-1000px'}, 5000);
+    $(".line").animate({top: '-1500px'}, 7000);
   }
+
+  $(document).keydown(function(e) {
+    if(e.keyCode == 37){
+      console.log("test");
+        $(".circle").animate({marginLeft: "-=15px"}, 5 );
+    }
+  });
   reset();
 });
