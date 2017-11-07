552dd39a147c2f20ee8fa5395da763a3c0fd3aa5
Tech-A03 <tech-a03@Tech-A03s-MacBook-Air-2.local>
2017-11-07 10:27:01 +0000

    Continuous scrolling

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 $(function() {
   console.log("DOM is loaded");
   function reset() {
-    $(".first").slideUp(3000, function() {
-      $(".second").slideUp(3000, reset);
-    });
+    $("div").animate({top: '-1000px'}, 4000, reset);
   }
   reset();
-})
+});
