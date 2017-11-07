93e00c868b062a207010d972b21c28582341ecf8
Tech-A03 <tech-a03@Tech-A03s-MacBook-Air-2.local>
2017-11-07 10:41:59 +0000

    Added ball

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function() {
   console.log("DOM is loaded");
   function reset() {
-    $("div").animate({top: '-1000px'}, 4000, reset);
+    $(".line").animate({top: '-1000px'}, 4000, reset);
   }
   reset();
 });
