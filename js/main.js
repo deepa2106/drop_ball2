a1baf348b474f535d88055daf96cdcf6b3d9fc76
Tech-A03 <tech-a03@Tech-A03s-MacBook-Air-2.local>
2017-11-07 10:49:23 +0000

    Positioned ball

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function() {
   console.log("DOM is loaded");
   function reset() {
-    $(".line").animate({top: '-1000px'}, 4000, reset);
+    $(".line").animate({top: '-1000px'}, 5000);
   }
   reset();
 });
