ddfd045c57bd4fec54b3b1b414f64c6867074430
Tech-A03 <tech-a03@Tech-A03s-MacBook-Air-2.local>
2017-11-08 09:39:47 +0000

    Added notes

--- js/main.js
+++ js/main.js
@@ -1,56 +1,67 @@
 $(function() {
-  console.log("DOM is loaded");
+  // Variable to keep track of score
   var score=0;
 
-
+  // Function to start game when game is over
   function reset() {
+    // Reload page to refresh div positions
     location.reload();
+    // Reset score to 0
     score = 0;
+    // Start animation for the game
     start();
   }
 
+  // Function to start the transition of the lines
   function start() {
     $(".line").animate({top: '-3000px'}, 24000);
   }
 
+  // Function to check if ball has collided with the line
   function collision($circle) {
+    // Stores lines that are closest to the ball
     var $line1 = $('.1')[score];
     var $line2 = $('.2')[score];
+    // Stores exact position of ball
     var circleLeft = $circle.offset().left;
     var circleTop = $circle.offset().top;
     var circleHeight = $circle.outerHeight(true);
     var circleWidth = $circle.outerWidth(true);
     var circleBottom = circleTop + circleHeight;
     var circleRight = circleLeft + circleWidth;
+    // Stores exact position of line on the  left
     var line1Left = $line1.offsetLeft;
     var line1Top = $line1.offsetTop;
     var line1Width = $line1.offsetWidth;
     var line1Height = $line1.offsetHeight;
     var line1Right = line1Left + line1Width;
     var line1Bottom = line1Top + line1Height;
+    // Stores exact position of line on the right
     var line2Left = $line2.offsetLeft;
     var line2Top = $line2.offsetTop;
     var line2Height = $line2.offsetHeight;
     var line2Bottom = line2Top + line2Height;
-    console.log("line1Top: " + line1Top);
-      console.log("line2Top: " + line2Top);
-      console.log("circleBottom: " + circleBottom);
+    // Returns score only if ball does not touch line
     if(line1Bottom <= circleTop && line2Bottom <= circleTop) {
+      // Adds 1 to score when it passes through the gap
       score++;
     } else if (circleBottom <= line1Top && circleBottom <= line2Top) {
       return score;
     } else if(circleLeft >= line1Right && circleRight <= line2Left) {
       return score;
     } else {
+      // Ends game when ball touches line
       alert("Game over");
       reset();
     }
   }
 
+  // Checks collision function every 1ms
   window.setInterval(function() {
     $('#result').text(collision($('.circle')))
   }, 1);
 
+  // Ball moves when you click left key or right key
   $(document).keydown(function(e) {
     if(e.keyCode == 37){
       $(".circle").animate({marginLeft: "-=35px"}, 1 );
@@ -59,7 +70,7 @@ $(function() {
       $(".circle").animate({marginLeft: "+=35px"}, 1 );
     }
   });
+
+  // Starts game
   start();
 });
-
-  var lines = {}
